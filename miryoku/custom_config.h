9efad72ff84b71c7c48e4d9a82b0d3c65cb9aa0b
Jeff Mann <110354170+becausebirds@users.noreply.github.com>
2025-01-10 15:24:49 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define COMBO_MUST_TAP_MS 100
 
+/ {
+    combos {
+        compatible = "zmk,combos";
+        combo_unlock {
+            timeout-ms = <50>;
+            key-positions = <30 31>;
+            bindings = <&sl 8>;
+        };
+    };
+};
