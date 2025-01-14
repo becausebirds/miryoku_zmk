ed61580650b289bb9aec702734c54b03809587cf
Jeff Mann <110354170+becausebirds@users.noreply.github.com>
2025-01-14 11:00:01 -0600

    Update corne.h

--- miryoku/mapping/42/corne.h
+++ miryoku/mapping/42/corne.h
@@ -33,8 +33,3 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
-
-&hid_trackball_interface {
-    automouse-layer = <5>;
-    automouse-layer-timeout-ms = <600>;
-};
