bada60a9bc3edd36ad5ffd202cf4c7ec64188165
Jeff Mann <110354170+becausebirds@users.noreply.github.com>
2025-01-22 09:18:47 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,7 +3,7 @@
 
 #define MIRYOKU_LAYER_MOUSE \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &tb_bootloader,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &tb_cyc_dpi,              U_NA,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
-U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     &tb_tg_scroll,              &tb_mo_scroll,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &tb_cyc_dpi,              &tb_mo_scroll,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
+U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     &tb_tg_scroll,              U_NP,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
